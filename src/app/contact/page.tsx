48cd847663c5902a23532f8db6984d4eacdb3433
GabrielBueno200 <gabrielbueno200@gmail.com>
2023-05-28 01:22:03 -0300

    added blank portfolio and contact pages

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,6 @@
+import PageContent from '../components/PageContent'
+
+const Contact = () => <PageContent title="Contact">{}</PageContent>
+
+export default Contact
+
---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,6 @@
+import PageContent from '../components/PageContent'
+
+const Portfolio = () => <PageContent title="Portfolio">{}</PageContent>
+
+export default Portfolio
+
