9977bf16c9938d1a8e393bad9ca82d2114103f5f
GabrielBueno200 <gabrielbueno200@gmail.com>
2023-05-29 22:57:58 -0300

    added responsive menu

--- src/app/components/NavBar/index.tsx
+++ src/app/components/NavBar/index.tsx
@@ -2,6 +2,8 @@
 
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import { GiHamburgerMenu } from 'react-icons/gi'
 
 const NavBar = () => {
   const pathname = usePathname()
@@ -9,32 +11,57 @@ const NavBar = () => {
   const isActive = (activePath: string) =>
     pathname === activePath ? 'text-white-green' : 'hover:text-zinc-400'
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  useEffect(() => {
+    setIsMenuOpen(false)
+  }, [pathname])
+
+  const handleToggleMenuOpen = () => setIsMenuOpen(!isMenuOpen)
+
   return (
-    <nav className="flex gap-6 bg-black uppercase text-white h-16 p-4 items-center justify-end text-xl">
-      <Link className={`${isActive('/')} transition-all`} href="/">
-        Home
-      </Link>
-      <Link className={`${isActive('/about')} transition-all`} href="/about">
-        About
-      </Link>
-      <Link
-        className={`${isActive('/experience')} transition-all`}
-        href="/experience"
+    <nav>
+      <div
+        className="navbar-toggler bg-black p-2 md:hidden border-b-gray-600"
+        onClick={handleToggleMenuOpen}
       >
-        Experience
-      </Link>
-      <Link
-        className={`${isActive('/portfolio')} transition-all`}
-        href="/portfolio"
+        <GiHamburgerMenu color="#C1F2B6" size={30} />
+      </div>
+
+      <div
+        className={`
+          ${
+            !isMenuOpen ? 'hidden' : 'flex'
+          } flex-col flex-wrap gap-4 md:flex-row md:flex-nowrap md:gap-6 bg-black uppercase text-white
+            lg:h-16 p-4 lg:items-center md:justify-end text-md md:text-xl
+            md:flex
+        `}
       >
-        Portfolio
-      </Link>
-      <Link
-        className={`${isActive('/contact')} transition-all`}
-        href="/contact"
-      >
-        Contact
-      </Link>
+        <Link className={`${isActive('/')} transition-all`} href="/">
+          Home
+        </Link>
+        <Link className={`${isActive('/about')} transition-all`} href="/about">
+          About
+        </Link>
+        <Link
+          className={`${isActive('/experience')} transition-all`}
+          href="/experience"
+        >
+          Experience
+        </Link>
+        <Link
+          className={`${isActive('/portfolio')} transition-all`}
+          href="/portfolio"
+        >
+          Portfolio
+        </Link>
+        <Link
+          className={`${isActive('/contact')} transition-all`}
+          href="/contact"
+        >
+          Contact
+        </Link>
+      </div>
     </nav>
   )
 }
