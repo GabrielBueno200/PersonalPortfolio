53216d75836d325b361ef41c63b4cb652a55abe1
GabrielBueno200 <gabrielbueno200@gmail.com>
2023-05-29 22:24:55 -0300

    added responsive home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 const Home = () => (
   <main
-    className="bg-secondary-green bg-right-bottom bg-no-repeat bg-50% flex items-center"
+    className="bg-secondary-green bg-right-bottom bg-no-repeat bg-50% flex items-center justify-center md:justify-normal"
     style={{ backgroundImage: "url('assets/me.png')" }}
   >
-    <div className="text-primary-green bg-black w-fit px-10 pb-10 pt-24 ml-20 flex flex-col">
+    <div className="text-primary-green bg-black w-fit px-10 pb-10 pt-12 -mt-24 md:mt-0 md:pt-24 md:ml-20 flex flex-col">
       <span className="w-fit inline-block">
         <h2 className="text-xl typewriter">Hi, my name is</h2>
       </span>
       <span className="w-fit inline-block">
-        <h1 className="text-7xl mt-6 mb-3 typewriter">Gabriel Bueno</h1>
+        <h1 className="text-5xl lg:text-7xl mt-6 mb-3 typewriter">
+          Gabriel Bueno
+        </h1>
       </span>
       <span className="w-fit inline-block">
         <h2 className="text-xl typewriter">
