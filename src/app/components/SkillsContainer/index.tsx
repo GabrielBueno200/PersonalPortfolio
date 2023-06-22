eedf5200bfbc44909855cc1369bc1242178cb029
Gabriel Bueno <56837996+GabrielBueno200@users.noreply.github.com>
2023-06-21 23:55:07 -0300

    fixed skills key

--- src/app/components/SkillsContainer/index.tsx
+++ src/app/components/SkillsContainer/index.tsx
@@ -13,10 +13,9 @@ const SkillsContainer = ({ category, skills }: ISkillsContainerProps) => (
   <div className="container">
     <h2 className="text-xl text-primary-green">{_.capitalize(category)}</h2>
     <div className="mt-2 flex flex-wrap gap-4 md:gap-5">
-      {skills.map((skill,) => (
-        <div className="flex max-w-[50px] flex-col items-center gap-2 text-center text-xs text-white">
+      {skills.map(skill => (
+        <div key={skill.name} className="flex max-w-[50px] flex-col items-center gap-2 text-center text-xs text-white">
           <Image
-            key={skill.name}
             src={skill.imagePath}
             width={60}
             height={60}
