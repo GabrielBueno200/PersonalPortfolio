d3ba703522c9a0e8218d288989e62f626fc31597
GabrielBueno200 <gabrielbueno200@gmail.com>
2023-06-24 20:56:18 -0300

    fixed skills page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,12 +9,12 @@ const About = () => {
 
   return (
     <PageContent title="About">
-      <div className="max-h-full md:flex md:gap-24">
-        <div className="who-container w-full lg:w-5/12 lg:self-center">
+      <div className="max-h-full lg:flex lg:gap-24">
+        <div className="who-container w-full lg:w-5/12">
           <h1 className="mb-6 text-2xl font-semibold text-primary-green lg:text-3xl">
             Who's this guy?
           </h1>
-          <span className="md:text-md text-white sm:text-sm">
+          <span className="lg:text-md text-white sm:text-sm">
             Hi, I'm Gabriel, a {myAge}-year-old software developer. I have a
             degree in Computer Science and I am also an IT technician. I possess
             skills in both backend and frontend software development. My
@@ -28,7 +28,7 @@ const About = () => {
           </span>
         </div>
 
-        <div className="skills-container mt-6 max-h-full flex-1 pb-10 md:-mt-6 md:pb-0 overflow-y-auto scrollbar-thin scrollbar-track-white-green scrollbar-thumb-primary-green scrollbar-track-rounded-full scrollbar-thumb-rounded-full sm:mb-5">
+        <div className="skills-container mt-6 max-h-full flex-1 pb-10 lg:-mt-6 lg:overflow-y-auto scrollbar-thin scrollbar-track-white-green scrollbar-thumb-primary-green scrollbar-track-rounded-full scrollbar-thumb-rounded-full">
           <h1 className="mb-6 text-2xl font-semibold text-primary-green lg:text-3xl">
             Skills
           </h1>
